8245a71f1a28a45a3e5de63bc45450e2bd28b3a9
SubhasishOmnify-HQ <subhasishsahoo4747@gmail.com>
2026-07-31 10:12:35 +0530

    Add: Basic Function in OpenCV

---
+++ 01_Image_Video_OpenCV_Python/02_Basic_Function.py
@@ -0,0 +1,36 @@
+import cv2
+# # import numpy as np
+
+# # conver to gray scale
+# img = cv2.imread("resources/lena.png")
+# img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# cv2.imshow("color", img)
+# cv2.imshow("gray", img_gray)
+# cv2.waitKey(0)
+
+
+### Convert to blur 
+
+# img = cv2.imread('resources/lena.png')
+# img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# img_blur = cv2.GaussianBlur(img_gray, (7,7), 0)
+# print("Color_img Shape: ", img.shape)
+# print("img_gray Shape: ", img_gray.shape)
+# cv2.imshow("Color_img", img)
+# cv2.imshow("Gray_img", img_gray)
+# cv2.imshow("img_blur", img_blur)
+# cv2.waitKey(0)
+
+### 3. Convert to cannyImg
+
+img = cv2.imread('resources/lena.png')
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_blur = cv2.GaussianBlur(img_gray, (7,7), 0)
+img_canny = cv2.Canny(img_blur, 100,100)
+print("Color_img Shape: ", img.shape)
+print("img_gray Shape: ", img_gray.shape)
+cv2.imshow("Color_img", img)
+cv2.imshow("Gray_img", img_gray)
+cv2.imshow("img_blur", img_blur)
+cv2.imshow("img_canny", img_canny)
+cv2.waitKey(0)
